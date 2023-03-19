35a57bd835daf34250340dae3e2b10ad0a4ec4ca
nathan-art <58117140+nathan-art@users.noreply.github.com>
2023-03-19 19:25:10 +0100

    Update script_parse_data_warehouse.py

--- script_parse_data_warehouse.py
+++ script_parse_data_warehouse.py
@@ -1,12 +1,15 @@
-#but de ce fichier : récupérer les données interessantes et construire la table de fait
+''' PROJECT OF DATA BASE EVOLVED -- 03-2023
+    This script generates the fact table csv and the date table csv.
+'''
 
 import csv
 import xml.etree.ElementTree as ET
+from datetime import datetime
 
-compt = 0
+compt_event_id = 0
 
 # Going through match_csv to sort the data
-with open('fact_table_before_script.csv', mode='r') as match_csv:
+with open('raw-aggregate.csv', mode='r') as match_csv:
     csv_reader = csv.DictReader(match_csv)
 
     # Create the tab for retrieve the values of some attributs
@@ -22,7 +25,7 @@ with open('fact_table_before_script.csv', mode='r') as match_csv:
     tab_date = []
     tab_league_id = []
 
-    #Ici, row est une ligne représentant un match
+    # Each iteration of the loop is focused on 1 match with several events
     for row in csv_reader:
 
         tree_goal = ET.fromstring(row["goal"])
@@ -34,7 +37,7 @@ with open('fact_table_before_script.csv', mode='r') as match_csv:
         tree_card = ET.fromstring(row["card"])
         tree_possession = ET.fromstring(row["possession"])
 
-        #tab_match_goals contient tous les buts de ce match
+        # tab_match_goals contains all goals of the current match, tab_match_shoton contains all shoton events of the current match, etc...
         tab_match_goals = []
         tab_match_shoton = []
         tab_match_shotoff = []
@@ -44,13 +47,13 @@ with open('fact_table_before_script.csv', mode='r') as match_csv:
         tab_match_card = []
         tab_match_possession = []
 
-        #ces lignes ajoutent les différentes valeurs des balises dans des tableaux,
-        #pour par la suite les traiter en tant que fichier xml
+        # We add the different values of the events in tabs
+        # These lines add the different values of the tags in tables to then process them as xml file
         for child in tree_shoton:
             tab_match_shoton.append(child)
 
         for child in tree_shotoff:
-            tab_match_shoton.append(child)
+            tab_match_shotoff.append(child)
 
         for child in tree_goal:
             tab_match_goals.append(child)
@@ -70,15 +73,19 @@ with open('fact_table_before_script.csv', mode='r') as match_csv:
         for child in tree_possession:
             tab_match_possession.append(child)
 
-        #cette partie selectionne les données utiles pour la table de faits dans les attributs listes du jeu de données initial
+        # This part selects useful data for the fact table in the list attributes of the initial dataset
         for elt in tab_match_shoton:
+            found = False
+
             for child in elt:
                 if child.tag == "team":
-                    #print(child.tag, " : ", child.text)
+                    found = True
                     tab_involved_team_id.append(child.text)
+            if not found:
+                tab_involved_team_id.append("")
 
-            tab_event_id.append(compt)
-            compt = compt + 1
+            tab_event_id.append(compt_event_id)
+            compt_event_id = compt_event_id + 1
 
             tab_type.append("shoton")
             tab_home_team_id.append(row["home_team_api_id"])
@@ -91,13 +98,17 @@ with open('fact_table_before_script.csv', mode='r') as match_csv:
             tab_league_id.append(row["league_id"])
 
         for elt in tab_match_goals:
+            found = False
+
             for child in elt:
                 if child.tag == "team":
-                    #print(child.tag, " : ", child.text)
+                    found = True
                     tab_involved_team_id.append(child.text)
+            if not found:
+                tab_involved_team_id.append("")
 
-            tab_event_id.append(compt)
-            compt = compt + 1
+            tab_event_id.append(compt_event_id)
+            compt_event_id = compt_event_id + 1
 
             tab_type.append("goal")
             tab_home_team_id.append(row["home_team_api_id"])
@@ -110,13 +121,17 @@ with open('fact_table_before_script.csv', mode='r') as match_csv:
             tab_league_id.append(row["league_id"])
 
         for elt in tab_match_shotoff:
+            found = False
+
             for child in elt:
                 if child.tag == "team":
-                    #print(child.tag, " : ", child.text)
+                    found = True
                     tab_involved_team_id.append(child.text)
+            if not found:
+                tab_involved_team_id.append("")
 
-            tab_event_id.append(compt)
-            compt = compt + 1
+            tab_event_id.append(compt_event_id)
+            compt_event_id = compt_event_id + 1
 
             tab_type.append("shotoff")
             tab_home_team_id.append(row["home_team_api_id"])
@@ -129,12 +144,18 @@ with open('fact_table_before_script.csv', mode='r') as match_csv:
             tab_league_id.append(row["league_id"])
 
         for elt in tab_match_foulcommit:
+            found = False
+
             for child in elt:
                 if child.tag == "team":
+                    found = True
                     tab_involved_team_id.append(child.text)
+            if not found:
+                tab_involved_team_id.append("")
 
-            tab_event_id.append(compt)
-            compt = compt + 1
+
+            tab_event_id.append(compt_event_id)
+            compt_event_id = compt_event_id + 1
 
             tab_type.append("foulcommit")
             tab_home_team_id.append(row["home_team_api_id"])
@@ -147,13 +168,17 @@ with open('fact_table_before_script.csv', mode='r') as match_csv:
             tab_league_id.append(row["league_id"])
 
         for elt in tab_match_corner:
+            found = False
+
             for child in elt:
                 if child.tag == "team":
-                    #print(child.tag, " : ", child.text)
+                    found = True
                     tab_involved_team_id.append(child.text)
+            if not found:
+                tab_involved_team_id.append("")
 
-            tab_event_id.append(compt)
-            compt = compt + 1
+            tab_event_id.append(compt_event_id)
+            compt_event_id = compt_event_id + 1
 
             tab_type.append("corner")
             tab_home_team_id.append(row["home_team_api_id"])
@@ -166,13 +191,17 @@ with open('fact_table_before_script.csv', mode='r') as match_csv:
             tab_league_id.append(row["league_id"])
 
         for elt in tab_match_cross:
+            found = False
+
             for child in elt:
                 if child.tag == "team":
-                    #print(child.tag, " : ", child.text)
+                    found = True
                     tab_involved_team_id.append(child.text)
+            if not found:
+                tab_involved_team_id.append("")
 
-            tab_event_id.append(compt)
-            compt = compt + 1
+            tab_event_id.append(compt_event_id)
+            compt_event_id = compt_event_id + 1
 
             tab_type.append("cross")
             tab_home_team_id.append(row["home_team_api_id"])
@@ -185,12 +214,17 @@ with open('fact_table_before_script.csv', mode='r') as match_csv:
             tab_league_id.append(row["league_id"])
 
         for elt in tab_match_card:
+            found = False
+
             for child in elt:
                 if child.tag == "team":
+                    found = True
                     tab_involved_team_id.append(child.text)
+            if not found:
+                tab_involved_team_id.append("")
 
-            tab_event_id.append(compt)
-            compt = compt + 1
+            tab_event_id.append(compt_event_id)
+            compt_event_id = compt_event_id + 1
 
             tab_type.append("card")
             tab_home_team_id.append(row["home_team_api_id"])
@@ -204,16 +238,25 @@ with open('fact_table_before_script.csv', mode='r') as match_csv:
 
         if len(tab_match_possession) > 0:
             elt_pos = tab_match_possession[-1]
-
+            foundAway = False
+            foundHome = False
             for child in elt_pos:
+                
+
                 if child.tag == "homepos":
+                    foundHome = True
                     tab_home_team_pos.append(child.text)
 
                 if child.tag == "awaypos":
+                    foundAway = True
                     tab_away_team_pos.append(child.text)
 
-                tab_event_id.append(compt)
-                compt = compt + 1
+            if (not foundHome) or (not foundAway):
+                tab_home_team_pos.append("") 
+                tab_away_team_pos.append("")
+
+            tab_event_id.append(compt_event_id)
+            compt_event_id = compt_event_id + 1    
 
             tab_type.append("possession")
             tab_home_team_id.append(row["home_team_api_id"])
@@ -224,27 +267,58 @@ with open('fact_table_before_script.csv', mode='r') as match_csv:
             tab_involved_team_id.append("")
             tab_league_id.append(row["league_id"])
 
+# Generates the CSV file of the Match_date dimension
+day_name = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+seen_date = []
+
+with open('match_date.csv', mode='w') as date_csv:
+    fieldnames = ['match_date','day','month','year','weekday']
+    writer = csv.DictWriter(date_csv, fieldnames=fieldnames)
+
+    writer.writeheader()
+
+
+    for i in range(len(tab_date)):
+        # We avoid duplicated dates
+        if tab_date[i] not in seen_date and int(tab_date[i][0:4]) >= 2015:
+            seen_date.append(tab_date[i])
+            day = int(tab_date[i][8:10])
+            month = int(tab_date[i][5:7])
+            year = int(tab_date[i][0:4])
+            weekday = day_name[datetime(year, month, day, 0, 0, 0).weekday()]
+            
+            writer.writerow({'match_date' : tab_date[i],
+                            'day' : day,
+                            'month' : month,
+                            'year' : year,
+                            'weekday' : weekday})
+
 # We overwrite the csv file with the retrieved data
 with open('fact_table_after_script.csv', mode='w', newline='') as new_csv:
     fieldnames = ['event_id', 'type','home_team_id','away_team_id','match_id','country_id','league_id','date','involved_team_id','home_team_pos','away_team_pos']
     writer = csv.DictWriter(new_csv, fieldnames=fieldnames)
 
     writer.writeheader()
-
+    
+    #This is used to count the amount of events written in the output file. This is important to limit the amount of tuples eventually added to the data warehouse,
+    #since our university doesn't allow us to insert an unlimited amount of tuples.
+    cpt = 0
     for i in range(len(tab_event_id)):
-        print("event id : ",tab_event_id[i])
-        print("i : ", i)
-
-        writer.writerow(
-            {'event_id': tab_event_id[i],
-             'type': tab_type[i],
-             'home_team_id': tab_home_team_id[i],
-             'away_team_id': tab_away_team_id[i],
-             'match_id': tab_match_id[i],
-             'country_id': tab_country_id[i],
-             'league_id': tab_league_id[i],
-             'date': tab_date[i],
-             'involved_team_id': tab_involved_team_id[i],
-             'home_team_pos': tab_home_team_pos[i],
-             'away_team_pos': tab_away_team_pos[i]
-             })
+        
+        # We choose only 4 leagues because SQLDeveloper can't have too many rows in the same table.
+        if (tab_league_id[i] == '4769' or tab_league_id[i] == '13274' or tab_league_id[i] == '15722' or tab_league_id[i] == '19694') and int(tab_date[i][0:4]) >= 2015:
+            cpt = cpt + 1
+            writer.writerow(
+                {'event_id': tab_event_id[i],
+                 'type': tab_type[i],
+                 'home_team_id': tab_home_team_id[i],
+                 'away_team_id': tab_away_team_id[i],
+                 'match_id': tab_match_id[i],
+                 'country_id': tab_country_id[i],
+                 'league_id': tab_league_id[i],
+                 'date': tab_date[i],
+                 'involved_team_id': tab_involved_team_id[i],
+                 'home_team_pos': tab_home_team_pos[i],
+                 'away_team_pos': tab_away_team_pos[i]
+                 })
+                
